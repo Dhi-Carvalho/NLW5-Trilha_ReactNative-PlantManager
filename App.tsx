cab49c6414966ba95cb34f8ab6194a6514cd8273
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-05-11 17:58:35 -0300

    Added the notification listener

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import React, { useEffect } from 'react';
 import {
   useFonts,
   Jost_300Light,
@@ -8,8 +7,11 @@ import {
   Jost_600SemiBold
   
 } from '@expo-google-fonts/jost'
+import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
 
 import Routes from './src/routes';
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -18,6 +20,28 @@ export default function App() {
     Jost_500Medium,
     Jost_600SemiBold
   });
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data)
+      }
+    )
+
+    return () => subscription.remove();
+  
+
+  // async function notifications() {
+  //   await Notifications.cancelAllScheduledNotificationsAsync();
+
+  //   const data = await Notifications.getAllScheduledNotificationsAsync();
+  //   console.log('NOTIFICAÇÕES AGENDADAS #####')
+  //   console.log(data)
+  // }
+
+  // notifications()
+  },[])
 
   if(!fontsLoaded)
     return <AppLoading/>
